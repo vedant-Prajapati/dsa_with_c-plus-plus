62b834600e6680cf5d4d9ab3c045cb993e0aca6d
Code_With_Vedant <127724117+vedant-Prajapati@users.noreply.github.com>
2025-06-09 23:47:26 +0530

    Day :- 8 Completed One Problem Statement In C++

---
+++ Problems With Ans/ques12.cpp
@@ -0,0 +1,20 @@
+// Count The Number of Digits fo Number n
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    int digits = 0;
+    while (n>0)
+    {
+        digits++;
+        n = n / 10;
+
+    }
+    cout << digits << endl;
+
+    return 0;
+}
